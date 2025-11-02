c9e3044e8b6d3f3b53fd6dc4b8cc88ab3780232a
Jaden Kai Rangel <jadenkrangel@gmail.com>
2025-11-02 13:13:04 -0600

    add nix files

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Ratatui Music Player Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alsa-lib
+            alsa-lib.dev
+            pkg-config
+          ];
+        };
+      }
+    );
+}
