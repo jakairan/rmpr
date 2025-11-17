59259f3c7a89d343efada98fb09a8abc407644bc
Jaden Kai Rangel <jadenkrangel@gmail.com>
2025-11-17 12:18:02 -0600

    new flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,11 @@
 {
   description = "Ratatui Music Player Rust";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  outputs = { nixpkgs, ... }: {
+    devShells.x86_64-linux.default = 
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [ alsa-lib pkg-config ];
+      };
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            alsa-lib
-            alsa-lib.dev
-            pkg-config
-          ];
-        };
-      }
-    );
 }
